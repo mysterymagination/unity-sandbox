121206da26cc0f1a15299e333ead846f7cba56df
Jeff Creswell <creswel2@gmail.com>
2025-07-10 22:31:29 -0400

    transform and roll out

--- Proto1_3D/Assets/Scripts/PlayerController.cs
+++ Proto1_3D/Assets/Scripts/PlayerController.cs
@@ -5,12 +5,13 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move vehicle forward
+        transform.Translate(0, 0, 1);
     }
 }
