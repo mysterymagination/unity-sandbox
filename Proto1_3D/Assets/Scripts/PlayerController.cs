47cfe787c023e019b728757cb698913806831354
Jeff Creswell <creswel2@gmail.com>
2025-07-16 00:26:09 -0500

    vectoring it up

--- Proto1_3D/Assets/Scripts/PlayerController.cs
+++ Proto1_3D/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // move vehicle forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward);
     }
 }
