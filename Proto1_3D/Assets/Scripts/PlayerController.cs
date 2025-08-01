9cf6d0b18c408b65b01fefa180e7b1dd0cffc8de
Jeff Creswell <creswel2@gmail.com>
2025-08-01 17:53:15 -0400

    input systeming it up

--- Proto1_3D/Assets/Scripts/PlayerController.cs
+++ Proto1_3D/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
     public float turnSpeed;
+    InputAction moveAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
@@ -18,5 +20,9 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // move vehicle laterally via the editor slider for turnSpeed
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        Debug.Log("Move action says " + moveValue[0] + "," + moveValue[1]);
+        // move vehicle laterally via user horizontal axis input
+        transform.Translate(Vector3.right * Time.deltaTime * moveValue[0]);
     }
 }
