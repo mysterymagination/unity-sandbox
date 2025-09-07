a233a947585e36661cd7c68f4450c99637f50921
Jeff Creswell <creswel2@gmail.com>
2025-09-07 04:23:34 -0500

    adding input action event handlers for accel

--- Proto1_3D/Assets/Scripts/PlayerController.cs
+++ Proto1_3D/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,6 +13,22 @@ public class PlayerController : MonoBehaviour
      * longer increases rate of acceleration.
      */
     public float acceleration = 10.0f;
+    /**
+     * Rate of change of acceleration in m/s^2, added to acceleration value per acceleration period with pedal held down.
+     */
+    public float acceleration_rate = 10.0f;
+    /**
+     * Rate of change of deceleration in m/s^2, subtracted from acceleration value per deceleration period with pedal released.
+     */
+    public float deceleration_rate = 10.0f;
+    /**
+     * Factor by which current acceleration will be multiplied per acceleration period with pedal held down.
+     */
+    public float acceleration_growth_factor = 1.5f;
+    /**
+     * Power to which current acceleration will be raised to generate new acceleration per acceleration period with pedal held down.
+     */
+    public float acceleration_growth_power = 1.0f;
     /**
      * Starting speed that increases/decreases with acceleration;
      * forward or backward (depth input axis) increases speed by 
@@ -26,6 +43,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        moveAction.started += OnPedalPressedEvent;
+        moveAction.performed += context => Debug.Log($"{context.action} performed");
+        moveAction.canceled += context => Debug.Log($"{context.action} canceled");
         accelerationEvent.AddListener(OnAccelerationPeriodEvent);
         ClockworkTasks clocks = gameObject.GetComponent<ClockworkTasks>();
         clocks.LaunchClock("TestEvent", accelerationEvent, 0, true, 5);
@@ -34,7 +54,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TODO: add input press/release event subs for moving forward/backward
+        
         // TODO: while depth axis movement pressed, increase speed by acceleration on a curve. while depth axis movement is unpressed, decrease speed by same.
 
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
@@ -43,6 +63,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * moveValue[1]);
         // yaw rotate vehicle via user horizontal axis input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveValue[0]);
+    }
+
+    void OnPedalPressedEvent(CallbackContext context)
+    {
+        Debug.Log($"{context.action} started as pedal is pressed");
     }
 
     void OnAccelerationPeriodEvent()
